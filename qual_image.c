2f4b4bc6bd253a6933947834ce8b79a46d0daa12
Martin Pollard <mp15@sanger.ac.uk>
2013-09-03 14:42:05 +0100

    First compilable draft of grayscale image creating part

--- qual_image.c
+++ qual_image.c
@@ -15,6 +15,7 @@
 // this program. If not, see L<http://www.gnu.org/licenses/>.
 
 #include <math.h>
+#include <stdbool.h>
 #include <stdlib.h>
 #include <string.h>
 #include "htslib/sam.h"
@@ -48,6 +49,7 @@ typedef struct parsed_readname {
 	char *machine_name;
 	int run_id;
 	int lane;
+	int full_tile;
 	int surface; // 0 based
 	int swath; // 0 based
 	int tile; // 0 based
@@ -71,6 +73,7 @@ static parsed_readname_t* parse_readname(const char* readname)
 	retval->run_id = atoi(token);
 	retval->lane = atoi(strsep(&toparse,":"));
 	token = strsep(&toparse,":");
+	retval->full_tile = atoi(token);
 	retval->surface = token[0] - '1';
 	retval->swath = token[1] - '1';
 	retval->tile = atoi(token+2)-1;
@@ -226,7 +229,7 @@ static void clear_isize(welford_stat_t tile_grid_isize[2][3][16])
 }
 
 
-static void bam_qualview_core(samFile* in, FILE* output[2][3][16], FILE* png[2][3][16] )
+static void bam_qualview_core(samFile* in, FILE* output[2][3][16], png_structp png_ptr[2][3][16], png_infop info_ptr[2][3][16] )
 {
 	bam1_t* b = bam_init1();
 	bam_hdr_t* hdr = sam_hdr_read(in);
@@ -250,18 +253,51 @@ static void bam_qualview_core(samFile* in, FILE* output[2][3][16], FILE* png[2][
 
 	clear_tq(tile_grid_qual);
 	clear_isize(tile_grid_isize);
-	while (sam_read1(in, hdr, b) >= 0) {
+	int full_tile;
+	png_structp current_png;
+	png_infop current_png_info;
+	png_bytepp current_bitmap;
+	
+	if (sam_read1(in, hdr, b) >= 0) {
+		parsed_readname_t* parse = parse_readname(bam_get_qname(b));
+		full_tile = parse->full_tile;
+		current_png = png_ptr[parse->surface][parse->swath][parse->tile];
+		current_png_info = info_ptr[parse->surface][parse->swath][parse->tile];
+		parsed_readname_destroy(parse);
+	
+		current_bitmap = (png_bytepp)calloc(2048, sizeof(png_bytep));
+		int i;
+		for (i = 0; i < 2048; ++i) {
+			current_bitmap[i] = (png_bytep)calloc(10000,png_get_rowbytes(current_png,current_png_info));
+		}
+	}
+	else {
+		bam_destroy1(b);
+		return;
+	}
+	do {
 		if (b->core.flag&BAM_FSECONDARY)
 			continue;
 		++count;
-		char* readname = bam_get_qname(b);
-		parsed_readname_t* parse = parse_readname(readname);
+		parsed_readname_t* parse = parse_readname(bam_get_qname(b));
 		int read = 0;
 		if ((b->core.flag&(BAM_FREAD1|BAM_FREAD2)) == (BAM_FREAD1|BAM_FREAD2)) {
 		} else if (b->core.flag&BAM_FREAD1) {
 			read = 1;
 		} else if (b->core.flag&BAM_FREAD2) {
 			read = 2;
+		}
+		if (full_tile != parse->full_tile)
+		{
+			png_write_image(current_png, current_bitmap);
+			png_write_end(current_png, current_png_info);
+			int i;
+			for (i = 0; i < 2048; ++i) {
+				memset(current_bitmap[i], 0, png_get_rowbytes(current_png,current_png_info)*10000);
+			}
+			full_tile = parse->full_tile;
+			current_png = png_ptr[parse->surface][parse->swath][parse->tile];
+			current_png_info = info_ptr[parse->surface][parse->swath][parse->tile];
 		}
 
 		++surface[parse->surface];
@@ -272,13 +308,21 @@ static void bam_qualview_core(samFile* in, FILE* output[2][3][16], FILE* png[2][
 			if ((b->core.flag&BAM_FPROPER_PAIR) != 0 && (b->core.isize > 0)) {
 				welford_add(&tile_grid_isize[parse->surface][parse->swath][parse->tile], (double)b->core.isize);
 				fprintf(output[parse->surface][parse->swath][parse->tile], "%d\t%d\t%d\t%d\n", bam_get_qual(b)[99], parse->x, parse->y, read);
+				
+				current_bitmap[parse->x][parse->y] = bam_get_qual(b)[99];
 			}
 
 			tile_grid_mq[parse->surface][parse->swath][parse->tile] += b->core.qual;
 		}
 		
 		parsed_readname_destroy(parse);
+	} while (sam_read1(in, hdr, b) >= 0);
+
+	int i;
+	for (i = 0; i < 2048; ++i) {
+		free(current_bitmap[i]);
 	}
+	free(current_bitmap);
 	
 	// summary
 	printf("Count: %d\n"
@@ -313,7 +357,7 @@ static void usage()
 	fprintf(stderr,"Usage information\n");
 }
 
-static bool open_files(const char* prefix, FILE* out[2][3][16], FILE* png[2][3][16], png_structp png_ptr[2][3][16])
+static bool open_files(const char* prefix, FILE* out[2][3][16], FILE* png[2][3][16], png_structp png_ptr[2][3][16], png_infop info_ptr[2][3][16])
 {
 	int surface, swath, tile;
 	for (surface = 0; surface < 2; ++surface) {
@@ -326,15 +370,14 @@ static bool open_files(const char* prefix, FILE* out[2][3][16], FILE* png[2][3][
 				out[surface][swath][tile] = fopen(buf, "w");
 				png[surface][swath][tile] = fopen(buf_png, "wb");
 				
-				png_structp png_ptr[surface][swath][tile] = png_create_write_struct
-				(PNG_LIBPNG_VER_STRING, (png_voidp)user_error_ptr,
-				 user_error_fn, user_warning_fn);
+				png_ptr[surface][swath][tile] = png_create_write_struct
+				(PNG_LIBPNG_VER_STRING, NULL, NULL, NULL);
 				
 				if (!png_ptr[surface][swath][tile])
 					return false;
 				
-				png_infop info_ptr = png_create_info_struct(png_ptr[surface][swath][tile]);
-				if (!info_ptr)
+				info_ptr[surface][swath][tile] = png_create_info_struct(png_ptr[surface][swath][tile]);
+				if (!info_ptr[surface][swath][tile])
 				{
 					png_destroy_write_struct(&png_ptr[surface][swath][tile],
 											 (png_infopp)NULL);
@@ -342,7 +385,7 @@ static bool open_files(const char* prefix, FILE* out[2][3][16], FILE* png[2][3][
 				}
 				
 				png_init_io(png_ptr[surface][swath][tile], png[surface][swath][tile]);
-				png_set_IHDR(png_ptr[surface][swath][tile], info_ptr, 2048,10000, 8, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
+				png_set_IHDR(png_ptr[surface][swath][tile], info_ptr[surface][swath][tile], 2048,10000, 8, PNG_COLOR_TYPE_GRAY, PNG_INTERLACE_NONE, PNG_COMPRESSION_TYPE_DEFAULT, PNG_FILTER_TYPE_DEFAULT);
 			}
 		}
 	}
@@ -374,14 +417,15 @@ int main(int argc, char *argv[])
 	FILE* out[2][3][16];
 	FILE* png[2][3][16];
 	png_structp png_ptr[2][3][16];
+	png_infop info_ptr[2][3][16];
 	while ((c = getopt(argc, argv, "")) >= 0) {
 		switch (c) {
 		}
 	}
 	if (optind+1 >= argc) usage();
 	in = sam_open(argv[optind], "rb", NULL);
-	open_files(argv[optind+1],out, png, png_ptr);
-	bam_qualview_core(in, out, png_ptr);
+	open_files(argv[optind+1],out, png, png_ptr, info_ptr);
+	bam_qualview_core(in, out, png_ptr, info_ptr);
 	sam_close(in);
 	close_files(out, png, png_ptr);
 	return 0;
--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-gcc -g -Wall  -O2 -o samtools_qual -I ../htslib qual_image.c libbam.a -Lbcftools -lbcf  ../htslib/libhts.a -lcurses  -lm -lz -lpthread 
+gcc -g -Wall  -O2 -o samtools_qual -I /opt/local/include/libpng15 -I ../htslib qual_image.c libbam.a -Lbcftools -lbcf -L/opt/local/lib  ../htslib/libhts.a -lcurses  -lm -lz -lpthread -lpng
